9ad1ad8ac263a17654c4948b72ca344b1219d104
Herschelle42 <16568797+Herschelle42@users.noreply.github.com>
2025-06-24 14:58:02 +1000

    fix parameter and use of host variable

--- fn_Test-ClusterSharedStorage.ps1
+++ fn_Test-ClusterSharedStorage.ps1
@@ -1,43 +1,46 @@
 function Test-ClusterSharedStorage {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory)]
-        [string]$ClusterName,
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]$Name,
 
         [string[]]$ExcludePatterns = @('local', 'Local')  # Default exclusions for local storage
     )
 
-    $cluster = Get-Cluster -Name $ClusterName -ErrorAction Stop
-    $hosts = Get-VMHost -Location $cluster -ErrorAction Stop
+    process {
+        $cluster = Get-Cluster -Name $Name -ErrorAction Stop
+        $vmhostList = Get-VMHost -Location $cluster -ErrorAction Stop
 
-    $datastoresPerHost = @{}
+        $datastoresPerHost = @{}
 
-    foreach ($host in $hosts) {
-        $datastores = Get-Datastore -VMHost $host | Where-Object {
-            $dsName = $_.Name
+        foreach ($vmhost in $vmhostList) {
+            $datastores = Get-Datastore -VMHost $vmhost | Where-Object {
+                $dsName = $_.Name
             ($ExcludePatterns | Where-Object { $dsName -match $_ }) -eq $null
+            }
+
+            $datastoresPerHost[$vmhost.Name] = $datastores.Name
         }
 
-        $datastoresPerHost[$host.Name] = $datastores.Name
-    }
+        # Build reference from first host
+        $referenceHost = $datastoresPerHost.Keys | Select-Object -First 1
+        $referenceStores = $datastoresPerHost[$referenceHost] | Sort-Object
 
-    # Build reference from first host
-    $referenceHost = $datastoresPerHost.Keys | Select-Object -First 1
-    $referenceStores = $datastoresPerHost[$referenceHost] | Sort-Object
+        $results = foreach ($hostName in $datastoresPerHost.Keys) {
+            $hostStores = $datastoresPerHost[$hostName] | Sort-Object
 
-    $results = foreach ($hostName in $datastoresPerHost.Keys) {
-        $hostStores = $datastoresPerHost[$hostName] | Sort-Object
+            $isMatch = ($hostStores -join '|') -eq ($referenceStores -join '|')
 
-        $isMatch = ($hostStores -join '|') -eq ($referenceStores -join '|')
-
-        [PSCustomObject]@{
-            HostName           = $hostName
-            MatchesReference   = $isMatch
-            DatastoreCount     = $hostStores.Count
-            MissingDatastores  = ($referenceStores | Where-Object { $_ -notin $hostStores }) -join ', '
-            ExtraDatastores    = ($hostStores | Where-Object { $_ -notin $referenceStores }) -join ', '
+            [PSCustomObject]@{
+                Cluster           = $Name
+                HostName          = $hostName
+                MatchesReference  = $isMatch
+                DatastoreCount    = $hostStores.Count
+                MissingDatastores = ($referenceStores | Where-Object { $_ -notin $hostStores }) -join ', '
+                ExtraDatastores   = ($hostStores | Where-Object { $_ -notin $referenceStores }) -join ', '
+            }
         }
-    }
 
-    return $results
+        return $results
+    }
 }
